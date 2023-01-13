cf13f3aeafb7e6ee1d6a5a39e45b6abe39aa8628
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-01-13 18:14:24 -0300

    applying global styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import GlobalStyles from "./styles/GlobalStyles";
+
 function App() {
   return (
     <>
       <h1>theraPrescription</h1>
+
+      <GlobalStyles />
     </>
   );
 }
